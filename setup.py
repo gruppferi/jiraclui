9937e8291a84735288665898969880cd4ca3e32e
Ferdows Shahryar <ferdows.shahryar@swarco.com>
2024-02-02 15:49:35 +0100

    update

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    try:
+        setup(
+            version="1.0.1",  # Set the desired version here
+            use_scm_version={"version_scheme": "no-guess-dev"},
+        )
+    except Exception as e:
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm, and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise e
